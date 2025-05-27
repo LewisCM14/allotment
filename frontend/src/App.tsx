ebbc139a2a0ec3a741a2b150710bb51c55d85eaf
LewisCM14 <lewiscmartin@live.co.uk>
2025-05-27 19:57:00 +0100

    prevent header from pulling down on refresh for mobile devices

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 				<AuthProvider>
 					<div className="flex flex-col h-screen">
 						<Header />
-						<div className="flex-1 flex items-start md:items-center justify-center overflow-y-auto pt-16 md:pt-24 pb-16">
+						<div className="flex-1 flex items-start md:items-center justify-center overflow-y-auto pt-16 md:pt-24 pb-16 overscroll-y-contain">
 							<AppRoutes />
 						</div>
 						<Footer />
