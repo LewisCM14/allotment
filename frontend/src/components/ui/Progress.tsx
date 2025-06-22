e1b20777abdbdd233d82019f74caa305185ebbb1
LewisCM14 <lewiscmartin@live.co.uk>
2025-06-22 20:29:41 +0100

    refactor progress

--- frontend/src/components/ui/Progress.tsx
+++ frontend/src/components/ui/Progress.tsx
@@ -20,7 +20,7 @@ function Progress({
 			<ProgressPrimitive.Indicator
 				data-slot="progress-indicator"
 				className="bg-primary h-full w-full flex-1 transition-transform"
-				style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+				style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
 			/>
 		</ProgressPrimitive.Root>
 	);
